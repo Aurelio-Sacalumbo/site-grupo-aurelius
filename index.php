a19257830c2dbb80a1149d648174ac769d1845cb
Aurélio Jamba Sacalumbo <aureliosacalumbo42@gmail.com>
2026-09-13 15:56:17 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 // Ficheiro: C:\xampp\htdocs\grupo-aurelius-pwa\index.php
 // Redireciona de forma invisível e instantânea para a raiz real do SaaS
-header("Location: Principal.php");
+header("Location: ./ Principal.php");
 exit();
 ?>
